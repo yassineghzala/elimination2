c258ba7a42cffaee6bdee7faf83b1f6502413d44
yassineghzala <ghzalayassin@gmail.com>
2023-09-27 18:49:32 +0100

    homepage1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './Homepage/homepage/homepage.component';
+import { HomesubjectComponent } from './Homepage/SubjectComponent/homesubject/homesubject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HomesubjectComponent
   ],
   imports: [
     BrowserModule,
